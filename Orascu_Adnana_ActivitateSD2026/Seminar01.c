1564698a014a808ffd863ea9872d6cd69fd926b1
Orascu Adnana Maria <orascuadnana24@stud.ase.ro>
2026-02-20 01:09:04 +0200

    citirea unui numar intreg

--- Orascu_Adnana_ActivitateSD2026/Seminar01.c
+++ Orascu_Adnana_ActivitateSD2026/Seminar01.c
@@ -3,7 +3,9 @@
 
 int main()
 {
-	printf("Salutare!\n");
-
+	printf("Salutare!\nIntroduceti un numar intreg: ");
+	int variabila = 0;
+	scanf_s("%d", &variabila);
+	printf("Ai introdus: %d", variabila);
 	return 0;
 }
